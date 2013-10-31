0288e17090e4ba4152789f18827de8430371fa88
Stephen Lauck <stephen.lauck@gmail.com>
2013-10-31 12:45:21 -0700

    initial commit of sml pipeline

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'pipeline'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures pipeline'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
