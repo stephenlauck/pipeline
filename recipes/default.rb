8ad8b887269a7a317795005cc89d00569b3244ad
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-01 15:52:33 -0700

    add server to user node

--- recipes/default.rb
+++ recipes/default.rb
@@ -60,7 +60,7 @@ template "#{node['jenkins']['server']['home']}/.berkshelf/config.json" do
     :validation_client_name => "chef-validator",
     :validation_key_path    => "#{node['jenkins']['server']['home']}/.berkshelf/validation.pem",
     :client_key_path        => "#{node['jenkins']['server']['home']}/.berkshelf/#{node['jenkins']['server']['user']}.pem",
-    :chef_node_name         => node['jenkins']['user']
+    :chef_node_name         => node['jenkins']['server']['user']
   )
 end
 
