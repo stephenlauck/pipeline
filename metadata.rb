ddeba4d98b19f2ff099cd1983fd62384b4e2fc4f
mauriciosilva <mauricio.silva@gmail.com>
2013-11-01 17:44:46 -0700

    adding apt dependency in order to lint

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description      'Installs/Configures pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
 depends "git"
 depends "jenkins"
