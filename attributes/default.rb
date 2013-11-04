aa89664b0809e1cad796e3586639f71f7279ba3d
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-04 15:08:06 -0800

    add dependency plugins

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
-default['jenkins']['server']['plugins'] = %w{github}
+
+# default['jenkins']['server']['plugins'] = %w{ssh-credentials credentials scm-api git-client git github-api github}
+default['jenkins']['server']['plugins'] = %w{scm-api git git-client github-api github}
 
 default['pipeline']['chef_server']['url'] = "CHEF_SERVER_URL"
 default['pipeline']['chef_server']['user_pem'] = "USER_PEM"
