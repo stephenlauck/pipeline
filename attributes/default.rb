229678248ec36b9f306765fecbf5449b5461f86e
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-05 10:36:10 -0800

    clean up comment

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,3 @@
-
-# default['jenkins']['server']['plugins'] = %w{ssh-credentials credentials scm-api git-client git github-api github}
 default['jenkins']['server']['plugins'] = %w{scm-api git git-client github-api github}
 
 default['pipeline']['chef_server']['url'] = "CHEF_SERVER_URL"
