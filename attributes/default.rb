4fe6e94ffa51136c6e0a1df008b9a31c645fdc93
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-04 16:09:25 -0800

    attributes for master github url/clone url

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,8 @@ default['jenkins']['server']['plugins'] = %w{scm-api git git-client github-api g
 default['pipeline']['chef_server']['url'] = "CHEF_SERVER_URL"
 default['pipeline']['chef_server']['user_pem'] = "USER_PEM"
 default['pipeline']['chef_server']['validation_pem'] = "VALIDATION_PEM"
+
+default['pipeline']['github']['repo_url'] = "GITHUB_URL_TO_BERKSFILE"
+default['pipeline']['github']['clone_url'] = "GIT_CLONE_URL_FOR_BERKSFILE"
+default['pipeline']['github']['branch'] = "*/master"
+
