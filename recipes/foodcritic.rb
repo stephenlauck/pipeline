d2e150bf2377582e5c377eb84d50c408e052c631
mauriciosilva <mauricio.silva@gmail.com>
2013-11-05 15:15:19 -0800

    Adding foodcritic
    added foodcritic dependencies
    adding -f flag to foodcritic command

--- recipes/foodcritic.rb
+++ recipes/foodcritic.rb
@@ -1,3 +1,7 @@
+# install some depends
+%w{libxslt-dev libxml2-dev}.each do |pkg|
+    package pkg
+end
 # install berkshelf gem
 gem_package "foodcritic" do
   gem_binary("/opt/chef/embedded/bin/gem")
