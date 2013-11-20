20fc8cf1343e6fd2e02cf7314fc2e0ab1dc7794f
mauriciosilva <mauricio.silva@gmail.com>
2013-11-20 13:49:21 -0800

    adding knife recipe

---
+++ recipes/knife.rb
@@ -0,0 +1,23 @@
+### 
+#
+# 1 - write out knife.rb
+# 2 - use chef server information to write file
+# 3 - abort if new chef-server information
+#
+#
+
+template "#{node['jenkins']['server']['home']}/.chef/knife.rb" do
+  source "knife.rb.erb"
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
+  mode 0644
+  variables(
+    :chef_server_url        => node['pipeline']['chef_server']['url'],
+    :validation_client_name => node['pipeline']['chef_server']['validation_client_name'],
+    :validation_key_path    => "#{node['jenkins']['server']['home']}/.berkshelf/#{node['pipeline']['chef_server']['validation_client_name']}.pem",
+    :client_key_path        => "#{node['jenkins']['server']['home']}/.berkshelf/#{node['jenkins']['server']['user']}.pem",
+    :chef_node_name         => node['jenkins']['server']['user']
+  )
+end
+
+
