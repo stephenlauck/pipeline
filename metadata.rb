7e6a99c2bc8d2171ac7ea28fea013eae2cd32de3
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-20 10:51:31 -0800

    update changelog and bump version to 1.0.0. Yay!

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'apt'
 depends "git"
