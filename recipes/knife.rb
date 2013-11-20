c8ba74a6daab02ad30b2fc3a1d67d3035652fcbd
mauriciosilva <mauricio.silva@gmail.com>
2013-11-20 14:26:41 -0800

    updating knife to add keys

--- recipes/knife.rb
+++ recipes/knife.rb
@@ -12,6 +12,28 @@ directory "#{node['jenkins']['server']['home']}/.chef" do
   mode 0755
 end
 
+
+####################################################################
+# write out chef server keys via attribute set by wrapper cookbook #
+####################################################################
+# user.pem
+file "#{node['jenkins']['server']['home']}/.chef/#{node['jenkins']['server']['user']}.pem" do
+  content node['pipeline']['chef_server']['client_key']
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
+  mode 0644
+  action :create
+end
+
+# validation.pem
+file "#{node['jenkins']['server']['home']}/.chef/#{node['pipeline']['chef_server']['validation_client_name']}.pem" do
+  content node['pipeline']['chef_server']['validation_key']
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
+  mode 0644
+  action :create
+end
+
 template "#{node['jenkins']['server']['home']}/.chef/knife.rb" do
   source "knife.rb.erb"
   owner node['jenkins']['server']['user']
@@ -20,8 +42,8 @@ template "#{node['jenkins']['server']['home']}/.chef/knife.rb" do
   variables(
     :chef_server_url        => node['pipeline']['chef_server']['url'],
     :validation_client_name => node['pipeline']['chef_server']['validation_client_name'],
-    :validation_key_path    => "#{node['jenkins']['server']['home']}/.berkshelf/#{node['pipeline']['chef_server']['validation_client_name']}.pem",
-    :client_key_path        => "#{node['jenkins']['server']['home']}/.berkshelf/#{node['jenkins']['server']['user']}.pem",
+    :validation_key_path    => "#{node['jenkins']['server']['home']}/.chef/#{node['pipeline']['chef_server']['validation_client_name']}.pem",
+    :client_key_path        => "#{node['jenkins']['server']['home']}/.chef/#{node['jenkins']['server']['user']}.pem",
     :chef_node_name         => node['jenkins']['server']['user']
   )
 end
