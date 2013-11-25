1b86438bba549c3f77ed93a9e2e7234256fc938b
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-25 13:42:44 -0800

    move build-essentials include

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,6 +20,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe 'build-essential'
 
 include_recipe 'pipeline::chef-zero' if node['pipeline']['chef-zero']
 
