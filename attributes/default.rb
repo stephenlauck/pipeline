af4bfff124c80807f1780c004b02f2999995fe3c
Stephen Lauck <stephen.lauck@gmail.com>
2013-11-26 17:33:22 -0800

    use lwrp sudo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
 default['jenkins']['server']['plugins']                      = %w{scm-api git git-client github-api github}
 default['jenkins']['http_proxy']['server_auth_method']       = 'basic'
 
+default['authorization']['sudo']['include_sudoers_d']        = true
+
 default['pipeline']['chef_server']['url']                    = "CHEF_SERVER_URL"
 default['pipeline']['chef_server']['client_key']             = "USER_PEM"
 default['pipeline']['chef_server']['validation_client_name'] = "VALIDATOR_NAME"
