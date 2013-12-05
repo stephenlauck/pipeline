6a5c097f37e4188d5273f75909ea20927847ab53
Stephen Lauck <stephen.lauck@gmail.com>
2013-12-05 15:52:41 -0800

    fix chef-zero bin path

--- recipes/chef-zero.rb
+++ recipes/chef-zero.rb
@@ -15,7 +15,7 @@ end
 
 template '/etc/init/chef-zero.conf' do
   source 'chef-zero.conf.erb'
-  variables({chef_embed_path: '/opt/chef/embedded/bin', chef_server_port: '8889'})
+  variables({chef_embed_path: '/opt/chef/bin', chef_server_port: '8889'})
   owner 'root'
   group 'root'
 end
