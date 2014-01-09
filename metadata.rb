0e09ae4a17894cb1ba6e17b4e6b333b5de1bed73
Stephen Lauck <stephen.lauck@gmail.com>
2014-01-09 15:58:19 -0800

    sudo chef-client in berkshelf job

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "lauck@opscode.com"
 license          'All rights reserved'
 description      'Installs/Configures pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.8'
+version          '1.0.9'
 
 depends 'apt'
 depends 'build-essential'
