8ff2465448efd31363088f68f9d74acfb3d0c853
Stephen Lauck <stephen.lauck@gmail.com>
2014-01-19 17:22:16 -0800

    pin cookbook versions for v1.1.0

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,12 @@ description      'Installs/Configures pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.12'
 
+depends 'yum', '= 2.4.4'
+depends 'runit', '= 1.4.0'
+depends 'nginx', '= 2.0.8'
+
 depends 'apt'
 depends 'build-essential'
-depends "git"
-depends "jenkins"
+depends "git", '= 2.8.4'
+depends "jenkins", '= 1.2.2'
 depends 'sudo'
