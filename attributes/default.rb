f63c89bcc598d3cb2499c1c8c402d5fe5c45bc11
Stephen Lauck <stephen.lauck@gmail.com>
2014-08-19 07:40:08 -0400

    update url for chef-repo
    add comments

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,13 @@
 default['pipeline']['jenkins']['plugins'] = %w{ scm-api git git-client github-api github }
 
+# use chef-zero url for default
 default['pipeline']['chef_server']['url'] = "http://0.0.0.0:80"
 default['pipeline']['chef_server']['node_name'] = "pipeline"
 
-default['pipeline']['chef-repo']['url'] = "https://github.com/stephenlauck/pipeline_chef.git"
+# use example chef-repo and poll master branch every minute by default
+default['pipeline']['chef-repo']['url'] = "https://github.com/stephenlauck/pipeline-example-chef-repo.git"
 default['pipeline']['chef-repo']['branch'] = "*/master"
 default['pipeline']['chef-repo']['polling'] = "* * * * *"
 
+# non-pipelined berks group for community cookbook install/upload
 default['pipeline']['berkshelf']['external']['group'] = "community"
